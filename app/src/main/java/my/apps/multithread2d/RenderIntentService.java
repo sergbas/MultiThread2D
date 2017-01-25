8b7d14e8ada98655b6bd509e54980ed47264d7b8
sbaskov <sergey.baskov@socialsys.ru>
2017-01-25 13:21:07 +0300

    runnable 4 threads

--- app/src/main/java/my/apps/multithread2d/RenderIntentService.java
+++ app/src/main/java/my/apps/multithread2d/RenderIntentService.java
@@ -25,6 +25,7 @@ public class RenderIntentService extends IntentService {
     private boolean isWorking = false;
     private Handler h;
     private ImageView image;
+    private Canvas canvas;
 
     public RenderIntentService() {
         super("RenderIntentService");
@@ -41,34 +42,63 @@ public class RenderIntentService extends IntentService {
 
         Log.e(TAG, "state:" + state + "; isWorking:" + isWorking);
 
+        drawRegion(0, 0, 320, 320);
+   }
+
+    private void drawRegion(int x1, int y1, int x2, int y2) {
         image = MainActivity.image;
+        MainActivity.bitmap = Bitmap.createBitmap(320, 320, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(MainActivity.bitmap);
 
-        MainActivity.bitmap = Bitmap.createBitmap(320, 480, Bitmap.Config.ARGB_8888);
-        final Canvas canvas = new Canvas(MainActivity.bitmap);
+        new MyRunnable(0, 0, 160, 160);
+        new MyRunnable(160, 0, 320, 160);
+        new MyRunnable(0, 160, 160, 320);
+        new MyRunnable(160, 160, 320, 320);
+    }
 
 
-        final Paint p = new Paint();
-        p.setAntiAlias(false);
-        p.setStyle(Paint.Style.FILL_AND_STROKE);
-        p.setStrokeWidth(1);
+    class MyRunnable implements Runnable {
+        Thread thread;
+        private int x1, y1, x2, y2;
 
-        for (int j = 0; j < 320; j++) {
-            for (int i = 0; i < 320; i++) {
-                int col = (int)System.currentTimeMillis();
-                p.setColor(col);
+        // Конструктор
+        MyRunnable(int x1, int y1, int x2, int y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
 
-                canvas.drawPoint(i, j, p);
+            thread = new Thread(this, "Поток");
+            thread.start();
+        }
 
-                if(i % 8 == 0 && j % 8 == 0) {
-                    Intent broadcastIntent = new Intent();
-                    broadcastIntent.setAction(MainActivity.ResponseReceiver.ACTION_RESP);
-                    broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
-                    broadcastIntent.putExtra(PARAM_OUT_MSG, i + ", " + j);
-                    sendBroadcast(broadcastIntent);
+        public void run() {
+            try {
+                final Paint p = new Paint();
+                p.setAntiAlias(false);
+                p.setStyle(Paint.Style.FILL_AND_STROKE);
+                p.setStrokeWidth(1);
+
+                for (int j = y1; j < y2; j++) {
+                    for (int i = x1; i < x2; i++) {
+                        int col = (int)System.currentTimeMillis();
+                        p.setColor(col);
+
+                        canvas.drawPoint(i, j, p);
+
+                        if(i % 4 == 0 && j % 4 == 0) {
+                            Intent broadcastIntent = new Intent();
+                            broadcastIntent.setAction(MainActivity.ResponseReceiver.ACTION_RESP);
+                            broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
+                            broadcastIntent.putExtra(PARAM_OUT_MSG, i + ", " + j);
+                            sendBroadcast(broadcastIntent);
+                        }
+                    }
                 }
+            } catch (Exception e) {
+                Log.i(TAG, "Второй поток прерван");
             }
         }
     }
-
 
 }
